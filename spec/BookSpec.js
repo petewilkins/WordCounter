d9b6417c1c84375c5a96980867d7e429693b1657
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-06-11 22:31:45 +0100

    countOccurnaces function implemented returning an object of key value pairs

--- spec/BookSpec.js
+++ spec/BookSpec.js
@@ -1,18 +1,28 @@
 describe('Book', function () {
   var book;
   beforeEach(function () {
-    book = new Book("This isn't a great story, is it?");
+    book = new Book("This isn't a story, or a poem, just some text!");
   });
 
-  describe('on Initialize', function () {
-    it('is empty by default', function () {
-      expect(book.getText()).toEqual("This isn't a great story, is it?");
+  describe('getText', function () {
+    it('returns the book text in lowercase', function () {
+      expect(book.getText()).toEqual("this isn't a story, or a poem, just some text!");
     });
   });
 
   describe('splitText', function () {
-    it('splits input text into an array of words', function () {
-      expect(book.splitText()).toEqual(['This', "isn't", 'a', 'great', 'story', 'is', 'it']);
+    it('splits input text into an array of words ignoring punctuation', function () {
+      book.getText();
+      expect(book.splitText()).toEqual(['this', "isn't", 'a', 'story', 'or', 'a', 'poem', 'just', 'some', 'text']);
+    });
+  });
+
+  describe('countOccurences', function () {
+    it('correctly counts the number of occurrences for each word', function () {
+      book.text = 'A story, a poem';
+      book.getText();
+      book.splitText();
+      expect(book.countOccurences()).toEqual({ a: 2, story: 1, poem: 1 });
     });
   });
 });
--- src/Book.js
+++ src/Book.js
@@ -2,13 +2,29 @@
 
 function Book(text) {
   this.text = text;
+  this.glossary = [];
 }
 
 Book.prototype.getText = function () {
-  return this.text;
+  return this.text = this.text.toLowerCase();
 };
 
 Book.prototype.splitText = function () {
-  // return this.text.split(' ');
-  return this.text.match(/(\b[^\s]+\b)/g);
+  this.glossary = this.text.match(/(\b[^\s]+\b)/g);
+  return this.glossary;
+};
+
+Book.prototype.countOccurences = function () {
+  var wordCounter = {};
+
+  for (var i = 0; i < this.glossary.length; i++) {
+    var word = this.glossary[i];
+    if (wordCounter.hasOwnProperty(word)) {
+      wordCounter[word] += 1;
+    } else {
+      wordCounter[word] = 1;
+    }
+  }
+
+  return wordCounter;
 };
